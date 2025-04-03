8aab9decbee3cf5f223d25c4f1312fe82b41d555
ModogTheDev <65304020+Modog500000@users.noreply.github.com>
2025-04-03 16:21:53 -0500

    :bug: Fix MouseHandelerMixin

--- common/src/main/java/org/modogthedev/superposition/mixin/MouseHandelerMixin.java
+++ common/src/main/java/org/modogthedev/superposition/mixin/MouseHandelerMixin.java
@@ -15,7 +15,7 @@ public class MouseHandelerMixin {
     @Inject(method = "onScroll", at = @At("HEAD"),cancellable = true)
     public void scroll(long windowPointer, double xOffset, double yOffset, CallbackInfo ci) {
         if (windowPointer == Minecraft.getInstance().getWindow().getWindow()) {
-            if (Minecraft.getInstance().player.getUseItem().is(SuperpositionItems.SIGNAL_SCOPE.get())) {
+            if (Minecraft.getInstance().player != null && Minecraft.getInstance().player.getUseItem().is(SuperpositionItems.SIGNAL_SCOPE.get())) {
                 SignalScopeRenderer.scroll((float) yOffset);
                 ci.cancel();
             }
